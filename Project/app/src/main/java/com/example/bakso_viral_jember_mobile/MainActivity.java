70c3382f91f4029820bb3721a8edc5282ec73d24
A_E41190139_LigaBayuHerdianto <71804899+LigaBayuHerdianto@users.noreply.github.com>
2021-07-11 04:49:33 +0700

    Login, Create Detail Pengiriman,

---
+++ Project/app/src/main/java/com/example/bakso_viral_jember_mobile/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.bakso_viral_jember_mobile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu. detail_pengiriman, menu );
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode) {
+        switch (selectedMode){
+            case R.id.LogOut:
+                Toast.makeText(this, "Selamat anda sudah Log Out", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+    }
+}
