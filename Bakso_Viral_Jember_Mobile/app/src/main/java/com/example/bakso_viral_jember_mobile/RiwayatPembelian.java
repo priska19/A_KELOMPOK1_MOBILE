9548fa43a3943f42d3be7b337d699d3346696f6a
NandaRaditiyaAkbar <nandaraditiya7@gmail.com>
2021-07-11 18:59:47 +0700

    Update RiwayatPembelian.java

--- Bakso_Viral_Jember_Mobile/app/src/main/java/com/example/bakso_viral_jember_mobile/RiwayatPembelian.java
+++ Bakso_Viral_Jember_Mobile/app/src/main/java/com/example/bakso_viral_jember_mobile/RiwayatPembelian.java
@@ -1,4 +1,39 @@
 package com.example.bakso_viral_jember_mobile;
 
-public class RiwayatPembelian {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RiwayatPembelian extends AppCompatActivity {
+
+    Button btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+
+        btn3 = findViewById(R.id.tombol3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RiwayatPembelian.this, MainActivity.class);
+                startActivity(i);
+            }
+
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
 }
