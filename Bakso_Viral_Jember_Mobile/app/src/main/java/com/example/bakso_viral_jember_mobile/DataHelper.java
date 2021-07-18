9a1cea8d623e63ecfc6488ddd28d4df63e298440
priska19 <priska360@gmail.com>
2021-07-18 15:48:28 +0700

    crud transaksi

--- Bakso_Viral_Jember_Mobile/app/src/main/java/com/example/bakso_viral_jember_mobile/DataHelper.java
+++ Bakso_Viral_Jember_Mobile/app/src/main/java/com/example/bakso_viral_jember_mobile/DataHelper.java
@@ -7,7 +7,7 @@ import android.util.Log;
 
 public class DataHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final String DATABASE_NAME = "baksoViral.db";
     private static final int DATABASE_VERSION = 1;
     public DataHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -15,15 +15,13 @@ public class DataHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE editprofile(no integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        String sql = "CREATE TABLE pesanan(nama text null, tgl_pesan text null, telpon text null, kecamatan text null, alamat text null);";
         Log.d("Data", "onCreate:" + sql);
         db.execSQL(sql);
-        sql = "INSERT INTO biodata (no, nama, tgl, jk, alamat) VALUES ('1', 'Firman Wahyudi', '2001-12-08', 'Laki-laki', 'Kapongan');";
+        sql = "INSERT INTO pesanan (nama, tgl_pesan, telpon, kecamatan, alamat) VALUES ('Liga Bayu', '18-7-2021','081234567890', 'Sumbersari', 'Jl. Mastrip');";
         db.execSQL(sql);
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-
     }
 }
--- Bakso_Viral_Jember_Mobile/app/src/main/java/com/example/bakso_viral_jember_mobile/RiwayatPembelian.java
+++ Bakso_Viral_Jember_Mobile/app/src/main/java/com/example/bakso_viral_jember_mobile/RiwayatPembelian.java
@@ -1,12 +1,19 @@
 package com.example.bakso_viral_jember_mobile;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -14,8 +21,14 @@ import com.example.bakso_viral_jember_mobile.listview.RecycleView;
 
 public class RiwayatPembelian extends AppCompatActivity {
 
-    Button btn3;
     DrawerLayout drawerLayout;
+    String[] order;
+    ListView listView01;
+    Menu menu;
+    String plhn_menu[] = {"Lihat Biodata", "Edit Biodata", "Hapus Biodata"};
+    protected Cursor cursor;
+    DataHelper dbCenter;
+    public static RiwayatPembelian ma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +37,54 @@ public class RiwayatPembelian extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
 
-        btn3 = findViewById(R.id.tombol3);
-        btn3.setOnClickListener(new View.OnClickListener() {
+        Button acBack = findViewById(R.id.btBack);
+        acBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(RiwayatPembelian.this, MainActivity.class);
-                startActivity(i);
+            public void onClick(View arg0) {
+                Intent intent = new Intent(RiwayatPembelian.this, Detail_Pengiriman.class);
+                startActivity(intent);
             }
         });
+        ma = this;
+        dbCenter = new DataHelper(this);
+        RefreshList();
+    }
+
+    public void RefreshList() {
+        SQLiteDatabase db = dbCenter.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM pesanan", null);
+        order = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for (int cc = 0; cc < cursor.getCount(); cc++) {
+            cursor.moveToPosition(cc);
+            order[cc] = cursor.getString(1).toString();
+        }
+        listView01 = findViewById(R.id.listView1);
+        listView01.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, order));
+        listView01.setSelected(true);
+        listView01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final String selection = order[position];
+                final CharSequence[] dialogItem = {"Lihat Order"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(RiwayatPembelian.this);
+                builder.setTitle("Pilihan");
+                builder.setItems(dialogItem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        switch (item) {
+                            case 0:
+                                Intent i = new Intent(RiwayatPembelian.this, Checkout.class);
+                                i.putExtra("tgl_pesan", selection);
+                                startActivity(i);
+                                break;
+                        }
+                    }
+                });
+                builder.create().show();
+            }
+        });
+        ((ArrayAdapter)listView01.getAdapter()).notifyDataSetInvalidated();
     }
 
     //jangan diotak atik sampe bawah
